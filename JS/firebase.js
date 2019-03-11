f15fcda1b2bec29dfa86df08ff7c1eb9849fc789
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-03-11 12:12:34 +0100

    inizializzo l'ambiente backend con firebase

---
+++ JS/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA1zuoZRpRhF2FCJyq9sy9kRgA8zD1xyIw",
+    authDomain: "recepies-93c4c.firebaseapp.com",
+    databaseURL: "https://recepies-93c4c.firebaseio.com",
+    projectId: "recepies-93c4c",
+    storageBucket: "recepies-93c4c.appspot.com",
+    messagingSenderId: "645081290149"
+  };
+  firebase.initializeApp(config);
