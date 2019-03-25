d86e970ad1f3a3b5147a2f6361ecb591c1ca6b6b
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-03-25 08:29:26 +0100

    Recupero lezione del 18/03/2019

---
+++ CSS/index.js
@@ -0,0 +1,11 @@
+
+// JS FOR OUR INDEX
+
+var db = firebase.firestore();
+
+db.collection("recepies").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      //console.log(`${doc.id} => ${doc.data()}`);
+      console.log( doc.data() );
+  });
+});
